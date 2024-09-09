3950df9104b9026bb3f90c3eb3dd1519bf6728d0
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-09-09 20:20:34 +0200

    user form #6

--- src/form/UserForm.php
+++ src/form/UserForm.php
@@ -5,10 +5,14 @@ class UserForm {
     public function __construct() {}
 
     public function loginForm(Twig\Environment $twig):void {
-        echo $twig->render('userLogin.html.twig', []);
+        echo $twig->render('user/userLogin.html.twig', []);
     }
 
     public function registerForm(Twig\Environment $twig):void {
-        echo $twig->render('userRegister.html.twig', []);
+        echo $twig->render('user/userRegister.html.twig', []);
+    }
+
+    public function userEditForm(Twig\Environment $twig, array $context):void {   
+        echo $twig->render('user/userEditForm.html.twig', $context);
     }
 }
