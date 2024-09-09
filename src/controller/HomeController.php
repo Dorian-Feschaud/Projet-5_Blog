e933c26f431c3c3f0ca808514c479ca8a23f3d1f
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-09-09 20:19:23 +0200

    home controller #8

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -6,15 +6,14 @@ class HomeController {
 
     public function __construct(){}
 
-    public function defaultHomepage(Twig\Environment $twig):void {
-        echo $twig->render('defaultHome.html.twig', []);   
-    }
-
-    public function loggedInHomepage(Twig\Environment $twig):void {
-        $utils = new Utils();
-        $user_id = $utils->getIdUser();
-        $user_repository = new UserRepository();
-        $user = $user_repository->getUser($user_id);
-        echo $twig->render('loggedInHome.html.twig', ['user' => $user]);    
+    public function homepage(Twig\Environment $twig, bool $logged_in):void {
+        $user = null;
+        if ($logged_in) {
+            $utils = new Utils();
+            $user_id = $utils->getIdUser();
+            $user_repository = new UserRepository();
+            $user = $user_repository->getUser($user_id);
+        }
+        echo $twig->render('home.html.twig', ['logged_in' => $logged_in, 'user' => $user]);   
     }
 }
