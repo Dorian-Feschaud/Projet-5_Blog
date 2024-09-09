2c7842c4778766d0d6223e8ffaba4ca12e6fa581
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-09-09 20:20:25 +0200

    post form #4

--- src/form/PostForm.php
+++ src/form/PostForm.php
@@ -5,10 +5,10 @@ class PostForm {
     public function __construct() {}
 
     public function postNewForm(Twig\Environment $twig):void {
-        echo $twig->render('postNewForm.html.twig', []);
+        echo $twig->render('post/postNewForm.html.twig', []);
     }
 
     public function postEditForm(Twig\Environment $twig, array $context):void {   
-        echo $twig->render('postEditForm.html.twig', $context);
+        echo $twig->render('post/postEditForm.html.twig', $context);
     }
 }
