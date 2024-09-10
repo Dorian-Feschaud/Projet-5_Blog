9bc228473836c82c8e4c4de0098ae6f0dbc7ef45
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-09-10 16:24:24 +0200

    remove var logged_in #8

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -6,14 +6,7 @@ class HomeController {
 
     public function __construct(){}
 
-    public function homepage(Twig\Environment $twig, bool $logged_in):void {
-        $user = null;
-        if ($logged_in) {
-            $utils = new Utils();
-            $user_id = $utils->getIdUser();
-            $user_repository = new UserRepository();
-            $user = $user_repository->getUser($user_id);
-        }
-        echo $twig->render('home.html.twig', ['logged_in' => $logged_in, 'user' => $user]);   
+    public function homepage(Twig\Environment $twig):void {
+        echo $twig->render('home.html.twig', []);   
     }
 }
