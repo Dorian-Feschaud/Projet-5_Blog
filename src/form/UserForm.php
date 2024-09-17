e88d6262053c0328f0eb74901587e132cd7bed69
Dorian Feschaud <dorian.feschaud@gmail.com>
2024-09-17 16:42:41 +0200

    login error #6

--- src/form/UserForm.php
+++ src/form/UserForm.php
@@ -4,8 +4,14 @@ class UserForm {
 
     public function __construct() {}
 
-    public function loginForm(Twig\Environment $twig):void {
-        echo $twig->render('user/userLogin.html.twig', []);
+    public function loginForm(Twig\Environment $twig, array $data):void {
+        if (isset($data['error'])) {
+            $error = true;
+        }
+        else {
+            $error = false;
+        }
+        echo $twig->render('user/userLogin.html.twig', ['error' => $error]);
     }
 
     public function registerForm(Twig\Environment $twig):void {
